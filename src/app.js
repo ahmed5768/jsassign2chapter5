c31f518d691a66a14507d6ed2eb1ea0e46599003
ahmed5768 <101082079+ahmed5768@users.noreply.github.com>
2024-02-17 03:48:20 +0500

    done

---
+++ src/app.js
@@ -0,0 +1,106 @@
+// sum
+var sum1 = 3;
+var sum2 =  5;
+var total1 = sum1 + sum2;
+console.log (total1);
+document.write ("sum of 3 and 5 is " + total1 + "</br>");
+
+// Subtraction
+var sub1 = 3;
+var sub2 = 5;
+var total2 = sub1 - sub2;
+console.log (total2);
+document.write ("subtract of 3 and 5 is " + total2 + "</br>");
+
+// Multiplication
+var mul1 = 3;
+var mul2 = 5;
+var total3 = mul1 * mul2;
+console.log (total3);
+document.write ("multiplication of 3 and 5 is " + total3 + "</br>");
+
+// Division
+var div1 = 3;
+var div2 = 5;
+var total4 = div1 / div2;
+console.log (total4);
+document.write ("division of 3 and 5 is " + total4 + "</br>");
+
+// Modulus
+var mod1 = 3;
+var mod2 = 5;
+var total5 = mod1 % mod2;
+console.log (total5);
+document.write ("modulus of 3 and 5 is " + total5 + "</br>");
+
+// variable undifined
+var myName
+console.log (myName);
+document.write ( "Value after variable declaration is " + myName + "</br>");
+
+// initialize variable
+var num = 5;
+console.log (num);
+document.write ("initial value: " + num + "<br>");
+
+// increament value
+var num1 =++num;
+console.log (num1);
+document.write ("Value after increament is : " + num1 + "</br>");
+
+// Addition Value
+var num2 = num1 + 7;
+console.log (num2);
+document.write ("Value after addition is : " + num2 + "</br>");
+
+// Decrement Value
+var num3 = --num2;
+console.log (num3);
+document.write ("Value after decreament is : " + num3 + "</br>");
+
+// Dividing variable
+var num4 = num3 % 3
+console.log (num4);
+document.write ("the reminder is : " + num4 + "</br>");
+
+// movie ticket
+document.write ("<h1>Movie ticket</h1>");
+var ticketPkr = 600;
+var qty = 5;
+console.log (ticketPkr * qty);
+document.write ("Total Cost to buy " + qty + " tickets to a movie is " + ticketPkr * qty + "</br>");
+
+// Table
+document.write ("<h1>Table 0f 4</h1>");
+var tableOf4 = 4;
+console.log (4 * 1);
+console.log (4 * 2);
+console.log (4 * 3);
+console.log (4 * 4);
+console.log (4 * 5);
+console.log (4 * 6);
+console.log (4 * 7);
+console.log (4 * 8);
+console.log (4 * 9);
+console.log (4 * 10);
+document.write ("4 * 1 = 4" + "</br>")
+document.write ("4 * 2 = 8" + "</br>")
+document.write ("4 * 3 = 12" + "</br>")
+document.write ("4 * 4 = 16" + "</br>")
+document.write ("4 * 5 = 20" + "</br>")
+document.write ("4 * 6 = 24" + "</br>")
+document.write ("4 * 7 = 28" + "</br>")
+document.write ("4 * 8 = 32" + "</br>")
+document.write ("4 * 9 = 36" + "</br>")
+document.write ("4 * 10 = 40" + "</br>")
+
+// Temperature converter
+document.write ("<h1>Temperature converter</h1>");
+var celsius = 25;
+var fahrenheit = (celsius * 9 / 5) + 32;
+console.log (fahrenheit);
+var fahrenheit2 = 70;
+var celsius2 = (fahrenheit2 - 32) * 5 / 9;
+console.log (celsius2)
+document.write (celsius + "°C is " + fahrenheit + "°F" + "</br>")
+document.write (fahrenheit2 + "°F is " + celsius2 + "°C" + "</br>")
